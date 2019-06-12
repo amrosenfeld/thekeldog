cfb6b56f317869eca653eb67fae35816e1a9f236
thekeldog <thekeldog@gmail.com>
2019-06-12 11:02:38 -0700

    comments and code cleanup for League class

--- League.py
+++ League.py
@@ -1,14 +1,67 @@
+import sys
+
+
 class League:
+    """League class holds games in a season and has ability to build and
+    print league tables.
+
+    Attributes:
+        teams {(team_name: points)}: Dictionary of all teams and associated
+        points for the season.
+
+        table [(team_name, points)]: Sorted list of tuples, team name and
+        points for season. Tie-breaker is alphabetical order.
+
+        matches [string]: Raw text of matches throughout a season.
+
+    """
+
     def __init__(self):
         self.teams = {}
         self.table = []
+        self.matches = []
+
+    def get_matches(self):
+        """Fill matches with data from either stdin or from file passed
+        from stdin
+
+            Parameters:
+                None: implicit in function is an argument passed via command line
+                when program is run.
+
+            Return:
+                None: Function populates self.matches to be used later in
+                calculating match results.
+
+        """
+
+        # Input will come from stdin or file from command line
+        if len(sys.argv) > 1:
+            input_file = sys.argv[1]
+            with open(input_file, 'r') as file:
+                self.matches = [f.strip() for f in file]
+        else:
+            self.matches = [line.strip() for line in sys.stdin]
 
     def print_table(self):
+        """Print the formatted league table from sorted table list
+
+            Parameters:
+                table [(name, points)]: Sorted list of tuples of meta team data
+
+            Return:
+                none: Function prints league data in desired format
+
+        """
+        if not self.table:
+            return
         position_counter = 1
+        # to test for ties in league table
         last_team_points = 0
+        # keep track of ties for resuming league position
         tied_teams = 0
         first = True
-        for team,score in self.table:
+        for team, score in self.table:
             if not first and last_team_points != score:
                 position_counter += 1 + tied_teams
                 tied_teams = 0
@@ -20,18 +73,31 @@ class League:
             last_team_points = score
             first = False
 
-    def fill_league(self, matches):
-        """
-        matches [] - array list of individual matches with final scores
+    def fill_league(self):
+        """ Populate teams with league meta-data, sort and assign to table
+
+        Parameters:
+            matches []: array list of individual matches with final scores
+
+        Return:
+            null: function merely populates teams and sorts into table
+
         function will take all matches and populate the teams dictionary
-        with the correct points associated with each match outcome
+        with the correct points associated with each match outcome.
+        Function currently sorts twice, to ensure alphabetical tie breaker.
+        A more robust sort key could be used to make only 1 sort, but unsure
+        on computational benefit.
+
         """
-        for match in matches:
+        if not self.matches:
+            return
+
+        for match in self.matches:
             teams = match.split(',')
             # Some teams have two names, split on space once from the right
             home = teams[0].strip().rsplit(' ', maxsplit=1)
             away = teams[1].strip().rsplit(' ', maxsplit=1)
-            # Check/populate teams table with new teams
+            # Check/populate teams dict with new teams
             if home[0] not in self.teams:
                 self.teams[home[0]] = 0
             if away[0] not in self.teams:
--- league_builder.py
+++ league_builder.py
@@ -5,23 +5,11 @@ Taking in a text file, or plain text from stdin create a soccer league
 table and print the results.
 """
 
-import sys
 from League import League
 
-# To be filled once input source is determined
-all_matches = []
-
-# Input will come from stdin or file from command line
-if len(sys.argv) > 1:
-    # print('file provided in argument: ' + sys.argv[1])
-    input_file = sys.argv[1]
-    with open(input_file, 'r') as file:
-        all_matches = [f.strip() for f in file]
-else:
-    # print('stdin case!')
-    all_matches = [line.strip() for line in sys.stdin]
 
 league = League()
-league.fill_league(all_matches)
+league.get_matches()
+league.fill_league()
 league.print_table()
 
